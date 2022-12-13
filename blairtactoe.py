429c47d71e6e970eb12c76d17163c0d1b8678e27
MikeWolski <wolski17@gmail.com>
2022-12-12 21:29:12 -0500

    Added Brookie, Added restart, Added backgrounds

--- blairtactoe.py
+++ blairtactoe.py
@@ -1,13 +1,12 @@
 import pygame
 import pygame_gui
-import time
 
 pygame.init()
 
 white = (255, 255, 255)
 red = (255, 0, 0)
 black = (0, 0, 0)
-gray = (60, 60, 60)
+gray = (82, 82, 82)
 blue = (0, 0, 255)
 green = (0, 255, 0)
 yellow = (255,255,0)
@@ -15,6 +14,7 @@ purple = (138,43,226)
 orange = (255,165,0)
 pink = (255,105,180)
 fuchsia = (255,0,255)
+colors = [purple, black, red, blue, green, yellow, orange, pink, fuchsia, white]
 dis_width = 800
 dis_height = 800
 dis = pygame.display.set_mode((dis_width, dis_height), pygame.RESIZABLE)
@@ -33,38 +33,55 @@ def startscreen(xc, cc):
     manager.set_window_resolution((dis_width, dis_height))
     so = (dis_width + dis_height)/80
     title_font = pygame.font.SysFont("bahnschrift", int(so*4))
-    dis.fill(fuchsia)
+    bgcolor = fuchsia
+    dis.fill(bgcolor)
     rad = dis_width
     if dis_width > dis_height:
         rad = dis_height
     blairimg = pygame.image.load("blair.png")
     blairimg = pygame.transform.scale(blairimg, (so*10, so*10))
+    brookimg = pygame.image.load("brook.png")
+    brookimg = pygame.transform.scale(brookimg, (so*10, so*10))
+    i = -1
     
     def title(msg,color, y):
         mesg = title_font.render(msg, True, color)
         dis.blit(mesg, [(dis_width-mesg.get_rect().width)/2, y])
 
-    def circle(color):
-        pygame.draw.circle(dis, color, [dis_width*3/4, dis_height*1/3], rad/9, int(so))
+    def circle(cc):
+        pygame.draw.circle(dis, cc, [dis_width*3/4, dis_height*1/3], rad/9, int(so))
         pygame.display.update()
 
-    def ex(color):
-        pygame.draw.line(dis, color, [dis_width*1/6, dis_height*1/4], [dis_width*1/3, dis_height*7/16], int(so))
-        pygame.draw.line(dis, color, [dis_width*1/6, dis_height*7/16], [dis_width*1/3, dis_height*1/4], int(so))
+    def ex(xc):
+        pygame.draw.line(dis, xc, [dis_width*1/6, dis_height*1/4], [dis_width*1/3, dis_height*7/16], int(so))
+        pygame.draw.line(dis, xc, [dis_width*1/6, dis_height*7/16], [dis_width*1/3, dis_height*1/4], int(so))
         pygame.display.update()
 
     def blair(x, y):
         dis.blit(blairimg, (dis_width*x, dis_height*y))
-    
-    def blank(x, y):
-        pygame.draw.rect(dis, fuchsia, [dis_width*x, dis_height*y, dis_width*2/8, dis_height*2.4/8])
         pygame.display.update()
 
-    ex(xc)
-    circle(cc)
+    def brook(x, y):
+        dis.blit(brookimg, (dis_width*x, dis_height*y))
+        pygame.display.update()
+    
+    def blank(x, y):
+        pygame.draw.rect(dis, bgcolor, [dis_width*x, dis_height*y, dis_width*2/8, dis_height*2.4/8])
+        pygame.display.update()
+    if xc == black:
+        blank(1/8, 1/5)
+        blair(1/8, 1/5)
+    else:
+        ex(xc)
+    if cc == black:
+        blank(5/8, 1/5)
+        brook(5/8, 1/5)
+    else:
+        circle(cc)
 
     box = {
     "start_button": pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/2-so*5, dis_height*1/3), (so*10, so*2)), text="Start", manager=manager),
+    "background_button": pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/2-so*5, dis_height*7/8), (so*10, so*2)), text="Background", manager=manager),
     "red_button": pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/4-so*5, dis_height*1/2), (so*10, so*2)), text="Red", manager=manager),
     "green_button": pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/4-so*5, dis_height*1/2+so*2), (so*10, so*2)), text="Green", manager=manager),
     "blue_button" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/4-so*5, dis_height*1/2+so*4), (so*10, so*2)), text="Blue", manager=manager),
@@ -78,12 +95,12 @@ def startscreen(xc, cc):
     "yellow_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*6), (so*10, so*2)), text="Yellow", manager=manager),
     "purple_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*8), (so*10, so*2)), text="Purple", manager=manager),
     "white_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*10), (so*10, so*2)), text="White", manager=manager),
-    "blair_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*12), (so*10, so*2)), text="BLAIR", manager=manager)
+    "brook_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*12), (so*10, so*2)), text="BROOK", manager=manager)
     }
 
     while True:
         time_delta = clock.tick(60)/1000.0
-        title("BLAIR-TAC-TOE", purple, 0)
+        title("BLAIR-TAC-TOE", gray, 0)
         manager.draw_ui(dis)
         pygame.display.update()
         for event in pygame.event.get():
@@ -98,13 +115,14 @@ def startscreen(xc, cc):
                     rad = dis_height
                 so = (dis_width + dis_height)/80
                 title_font = pygame.font.SysFont("bahnschrift", int(so*4))
-                dis.fill(fuchsia)
+                dis.fill(bgcolor)
                 manager.clear_and_reset()
                 manager.set_window_resolution((dis_width, dis_height))
-                title("BLAIR-TAC-TOE", purple, 0)
+                title("BLAIR-TAC-TOE", gray, 0)
                 so = (dis_width + dis_height)/80
                 box = {
                 "start_button": pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/2-so*5, dis_height*1/3), (so*10, so*2)), text="Start", manager=manager),
+                "background_button": pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/2-so*5, dis_height*7/8), (so*10, so*2)), text="Background", manager=manager),
                 "red_button": pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/4-so*5, dis_height*1/2), (so*10, so*2)), text="Red", manager=manager),
                 "green_button": pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/4-so*5, dis_height*1/2+so*2), (so*10, so*2)), text="Green", manager=manager),
                 "blue_button" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*1/4-so*5, dis_height*1/2+so*4), (so*10, so*2)), text="Blue", manager=manager),
@@ -118,43 +136,60 @@ def startscreen(xc, cc):
                 "yellow_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*6), (so*10, so*2)), text="Yellow", manager=manager),
                 "purple_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*8), (so*10, so*2)), text="Purple", manager=manager),
                 "white_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*10), (so*10, so*2)), text="White", manager=manager),
-                "blair_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*12), (so*10, so*2)), text="BLAIR", manager=manager)
+                "brook_buttono" : pygame_gui.elements.UIButton(relative_rect=pygame.Rect((dis_width*3/4-so*5, dis_height*1/2+so*12), (so*10, so*2)), text="BROOK", manager=manager)
                 }
                 ex(xc)
                 circle(cc)
                 blairimg = pygame.image.load("blair.png")
                 blairimg = pygame.transform.scale(blairimg, (so*10, so*10))
+                brookimg = pygame.image.load("brook.png")
+                brookimg = pygame.transform.scale(brookimg, (so*10, so*10))
                 pygame.display.update()
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["start_button"]:
-                    gameloop(cc, xc)
+                    gameloop(cc, xc, bgcolor)
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == box["background_button"]:
+                    i+=1
+                    if i == 10:
+                        i = 0
+                    bgcolor = colors[i]
+                    dis.fill(bgcolor)
+                    if xc == black:
+                        blank(1/8, 1/5)
+                        blair(1/8, 1/5)
+                    else:
+                        ex(xc)
+                    if cc == black:
+                        blank(5/8, 1/5)
+                        brook(5/8, 1/5)
+                    else:
+                        circle(cc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["red_button"]:
                     xc = red
                     blank(1/8, 1/5)
-                    ex(red)
-                    pygame.display.update()
+                    ex(xc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["green_button"]:
                     xc = green
                     blank(1/8, 1/5)
-                    ex(green)
-                    pygame.display.update()
+                    ex(xc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["blue_button"]:
                     xc = blue
                     blank(1/8, 1/5)
-                    ex(blue)
+                    ex(xc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["yellow_button"]:
                     xc = yellow
                     blank(1/8, 1/5)
-                    ex(yellow)
+                    ex(xc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["purple_button"]:
                     xc = purple
                     blank(1/8, 1/5)
-                    ex(purple)
+                    ex(xc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["white_button"]:
                     xc = white
@@ -169,54 +204,56 @@ def startscreen(xc, cc):
                 if event.ui_element == box["red_buttono"]:
                     cc = red
                     blank(5/8, 1/5)
-                    circle(red)
+                    circle(cc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["green_buttono"]:
                     cc = green
                     blank(5/8, 1/5)
-                    circle(green)
+                    circle(cc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["blue_buttono"]:
                     cc = blue
                     blank(5/8, 1/5)
-                    circle(blue)
+                    circle(cc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["yellow_buttono"]:
                     cc = yellow
                     blank(5/8, 1/5)
-                    circle(yellow)
+                    circle(cc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["purple_buttono"]:
                     cc = purple
                     blank(5/8, 1/5)
-                    circle(purple)
+                    circle(cc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == box["white_buttono"]:
                     cc = white
                     blank(5/8, 1/5)
-                    circle(white)
+                    circle(cc)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
-                if event.ui_element == box["blair_buttono"]:
+                if event.ui_element == box["brook_buttono"]:
                     cc = black
                     blank(5/8, 1/5)
-                    blair(5/8, 1/5)
+                    brook(5/8, 1/5)
             manager.process_events(event)
         manager.update(time_delta)
-def gameloop(cc, xc):
+
+def gameloop(cc, xc, bgcolor):
     dis_width = dis.get_width()
     dis_height = dis.get_height()
     so = (dis_width + dis_height)/240
-    dis.fill(fuchsia)
+    dis.fill(bgcolor)
+    pygame.display.update()
     font_style = pygame.font.SysFont("bahnschrift", int(so*8))
     game_over = False
-    xcolor = xc
-    ocolor = cc
-    boardcolor = purple
+    boardcolor = gray
     rad = dis_width
     if dis_width > dis_height:
         rad = dis_height
     blairimg = pygame.image.load("blair.png")
     blairimg = pygame.transform.scale(blairimg, (rad/8, rad/8))
+    brookimg = pygame.image.load("brook.png")
+    brookimg = pygame.transform.scale(brookimg, (rad/8, rad/8))
 
     def board():
         pygame.draw.rect(dis, boardcolor, [(dis_width*1/8)-(so/2), 0, so, dis_height])
@@ -236,20 +273,25 @@ def gameloop(cc, xc):
         pygame.display.update()
 
     def circle(x, y):
-        pygame.draw.circle(dis, ocolor, [dis_width*x, dis_height*y], rad/18, int(so))
+        pygame.draw.circle(dis, cc, [dis_width*x, dis_height*y], rad/18, int(so))
         pygame.display.update()
 
     def ex(x, y, a, b):
-        pygame.draw.line(dis, xcolor, [dis_width*x, dis_height*a], [dis_width*y, dis_height*b], int(so))
-        pygame.draw.line(dis, xcolor, [dis_width*x, dis_height*b], [dis_width*y, dis_height*a], int(so))
+        pygame.draw.line(dis, xc, [dis_width*x, dis_height*a], [dis_width*y, dis_height*b], int(so))
+        pygame.draw.line(dis, xc, [dis_width*x, dis_height*b], [dis_width*y, dis_height*a], int(so))
         board()
         pygame.display.update()
 
     def blair(x, y):
         dis.blit(blairimg, (dis_width*x, dis_height*y))
+        pygame.display.update()
+
+    def brook(x, y):
+        dis.blit(brookimg, (dis_width*x, dis_height*y))
+        pygame.display.update()
 
     def blank(x, y):
-        pygame.draw.rect(dis, fuchsia, [dis_width*x, dis_height*y, dis_width*1/8, dis_height*1/8])
+        pygame.draw.rect(dis, bgcolor, [dis_width*x, dis_height*y, dis_width*1/8, dis_height*1/8])
         board()
         pygame.display.update()
 
@@ -343,7 +385,7 @@ def gameloop(cc, xc):
                         a.taken += 1
                         blank(a.x, a.y)
                         if cc == black:
-                            blair(a.x, a.y)
+                            brook(a.x, a.y)
                             board()
                             pygame.display.update()
                         else:
@@ -354,15 +396,16 @@ def gameloop(cc, xc):
                         blank(a.x, a.y)
                         pygame.display.update()
     board()
-
+    
+    pygame.display.update()
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game_over = True
+                startscreen(xc, cc)
             if event.type==pygame.VIDEORESIZE:
                 dis_height = dis.get_height()
                 dis_width = dis.get_width()
-                dis.fill(fuchsia)
+                dis.fill(bgcolor)
                 a1 = area(0,0,0)
                 a2 = area(1/8,0,0)
                 a3 = area(2/8,0,0)
@@ -433,8 +476,11 @@ def gameloop(cc, xc):
                     rad = dis_height
                 blairimg = pygame.image.load("blair.png")
                 blairimg = pygame.transform.scale(blairimg, (rad/8, rad/8))
+                brookimg = pygame.image.load("brook.png")
+                brookimg = pygame.transform.scale(brookimg, (rad/8, rad/8))
                 board()
                 pygame.display.update()
+
             move(a1)
             move(a2)
             move(a3)
@@ -503,5 +549,4 @@ def gameloop(cc, xc):
         clock.tick(60)
     pygame.quit()
     quit()
-
 startscreen(xc, cc)
